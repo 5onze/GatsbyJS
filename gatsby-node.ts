29f83a34583ecf6d8c30bfc3b4b2479db9423d63
whirlpoolgreen <darkerhorn@gmail.com>
2023-09-19 21:33:02 +0900

    GATSBY - Creating a Project.

---
+++ gatsby-node.ts
@@ -0,0 +1,48 @@
+import { CreateWebpackConfigArgs } from "gatsby";
+
+const path = require(`path`);
+
+// Create blog pages dynamically
+/* exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
+  const result = await graphql(`
+    query {
+      allSamplePages {
+        edges {
+          node {
+            slug
+            title
+          }
+        }
+      }
+    }
+  `);
+  result.data.allSamplePages.edges.forEach((edge) => {
+    createPage({
+      path: `${edge.node.slug}`,
+      component: blogPostTemplate,
+      context: {
+        title: edge.node.title,
+      },
+    });
+  });
+}; */
+// Let plugins extend/mutate the site’s webpack configuration.
+exports.onCreateWebpackConfig = ({
+  getConfig,
+  actions,
+}: CreateWebpackConfigArgs) => {
+  const output = getConfig().output || {};
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        utils: path.resolve(__dirname, "src/utils"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+      },
+    },
+  });
+};
